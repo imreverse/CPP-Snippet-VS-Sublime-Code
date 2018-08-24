dcb0ea6c0ee4ac47852b39024018710751592f64
Aayush Tyagi <33152469+imreverse@users.noreply.github.com>
2018-08-25 04:42:11 +0530

    Update cpp.js

--- cpp.js
+++ cpp.js
@@ -1,17 +1,4 @@
 {
-	// Place your snippets for cpp here. Each snippet is defined under a snippet name and has a prefix, body and 
-	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
-	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the 
-	// same ids are connected.
-	// Example:
-	// "Print to console": {
-	// 	"prefix": "log",
-	// 	"body": [
-	// 		"console.log('$1');",
-	// 		"$2"
-	// 	],
-	// 	"description": "Log output to console"
-	// }
 	"Pre-written Code": {
 		"prefix": "imrvrs",
 		"body": [
@@ -57,10 +44,10 @@
 		  "#define fast std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)",
 		  "using namespace std;",
 		  "ll int gcd(ll int a, ll int b)",
-    	  "{",
-    	  "    if( a==0 ) return b;",
-    	  "    return gcd( b%a, a );",
-    	  "}",
+    	  	  "{",
+    	 	  "    if( a==0 ) return b;",
+    		  "    return gcd( b%a, a );",
+    		  "}",
 		  "ll int fast_power(ll int base, ll int power, ll int mod)",
 		  "{",
 		  "    ll int result = 1;",
